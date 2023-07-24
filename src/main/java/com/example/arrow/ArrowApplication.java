df69fe44e06025f882649fffdbe9f9c68e4c9b0c
JatinCapillary <jatin@capillarytech.com>
2023-07-24 13:42:24 +0530

    first commit

---
+++ src/main/java/com/example/arrow/ArrowApplication.java
@@ -0,0 +1,13 @@
+package com.example.arrow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArrowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArrowApplication.class, args);
+	}
+
+}
